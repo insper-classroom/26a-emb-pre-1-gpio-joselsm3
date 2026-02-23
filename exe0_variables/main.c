d18c18df518b66dd2c9c72da2a17e178bc9a9b73
joselsm3 <joselsm3@al.insper.edu.br>
2026-02-23 13:36:33 -0300

    ajuste

--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -6,13 +6,14 @@ void foo(int *a) {
 
 int main(void) {
     int a = 0;
-    int b = 0;
 
     while (1) {
         foo(&a);
         
         if (a > 5) {
-            b = 1;
+            break;
         }
     }
+
+    return 0;
 }
