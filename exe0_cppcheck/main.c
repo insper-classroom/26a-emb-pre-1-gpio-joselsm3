4a471ab6e3a81162f14c082c4062391a7ad4d48d
joselsm3 <joselsm3@al.insper.edu.br>
2026-02-23 13:31:12 -0300

    ajuste

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -13,10 +13,14 @@ int main(void) {
     int result = multiplyNumbers(a, b);
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+    int sum = 0;
+    for (int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
+        sum += arr[i];
     }
 
+    (void)arr;
+    (void)sum;
     (void)result;
 
     return 0;
