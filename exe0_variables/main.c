bb0bc83d836f071b2791fa9c606a8c86ae18bbf1
joselsm3 <joselsm3@al.insper.edu.br>
2026-02-23 13:34:28 -0300

    ajuste

--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -1,15 +1,15 @@
-#include "stdio.h"
+#include <stdio.h>
 
-static int a = 0; // var global
-static int b = 0; // var global
-
-void foo(void) {
-    a = a + 1; // acessa variavel global
+void foo(int *a) {
+    *a = *a + 1;
 }
 
 int main(void) {
+    int a = 0;
+    int b = 0;
+
     while (1) {
-        foo();
+        foo(&a);
         
         if (a > 5) {
             b = 1;
